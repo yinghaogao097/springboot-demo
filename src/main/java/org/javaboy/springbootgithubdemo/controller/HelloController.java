9865587e9c82a4b042d84e4e5a0da237e513073f
高莹豪 <yinghaogao097@gmail.com>
2023-03-14 20:33:40 +0800

    创建了一个接口 返回 hello github

---
+++ src/main/java/org/javaboy/springbootgithubdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.javaboy.springbootgithubdemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author：Achen
+ */
+@RestController
+public class HelloController {
+    public String hello() {
+        return "hello github";
+    }
+}
