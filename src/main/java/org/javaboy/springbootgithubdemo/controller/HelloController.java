c40d8b9cf0b713f54aef441c9dc9f57d686dc13f
高莹豪 <yinghaogao097@gmail.com>
2023-03-14 21:31:20 +0800

    修改了返回语句

--- src/main/java/org/javaboy/springbootgithubdemo/controller/HelloController.java
+++ src/main/java/org/javaboy/springbootgithubdemo/controller/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     public String hello() {
-        return "hello github";
+        return "hello github啊啊啊";
     }
 }
