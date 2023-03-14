aac3849966a6eafa9072c7094acd8f4785c0c8ef
高莹豪 <yinghaogao097@gmail.com>
2023-03-14 20:29:04 +0800

    Initial commit

---
+++ src/main/java/org/javaboy/springbootgithubdemo/SpringbootGithubDemoApplication.java
@@ -0,0 +1,13 @@
+package org.javaboy.springbootgithubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGithubDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaboy/springbootgithubdemo/SpringbootGithubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.springbootgithubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
